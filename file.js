ab1d98a0b8da34c29a2cc0fed5f45ea3aa89ba61
Pritam Kumar <77887792+pritamkr1@users.noreply.github.com>
2021-02-27 15:04:09 +0700

    Initial commit

---
+++ file.js
@@ -0,0 +1,39 @@
+var time = document.querySelector('.clock-container');
+
+
+
+function showtime(){
+    var mydate = new Date();
+   //time.innerHTML = mydate.toUTCString();
+ //time.innerHTML = mydate.toDateString();
+    var h = mydate.getHours();
+    var m = mydate.getMinutes();
+    var s = mydate.getSeconds();
+    var period = "AM";
+    if (h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        period = "PM";
+    }
+
+    if(h < 10){
+        h = "0" + h;
+    }
+    if(m < 10){
+        m = "0" + m;
+    }
+    if(s < 10){
+        s = "0" + s;
+    }
+    var timing = `${h} : ${m} : ${s}`;;
+    time.innerHTML = timing;
+
+
+    setTimeout(showtime, 1000);
+
+}
+
+showtime();
